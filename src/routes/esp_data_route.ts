6d622d3708f981e416e4cae084b7c046d11d3a17
Jefferson Vargas <jefferson.vargas@ciag.org.br>
2023-04-25 14:47:59 -0300

    Deleted files

--- src/routes/esp_data_route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-const espPostDataController = require('../controllers/post_data_from_esp');
-
-app.use('', espPostDataController);
-
-module.exports = app;
