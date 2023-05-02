f98128fc4b019a2f2d91a963e4e2616e3f1805ca
ゆいち <100085891+VictorKashima@users.noreply.github.com>
2023-05-02 19:39:05 -0300

    FIX: Updated code and added reference pin

--- DHT.ino
+++ DHT.ino
@@ -1,22 +1,24 @@
 #include <DHT.h>
 
-DHT dht(26, DHT11);
+#define DHTPIN 2          // Define o pino ao qual o sensor DHT11 está conectado (Pino D4)
+#define DHTTYPE DHT11     // Define o tipo de sensor DHT11
+DHT dht(DHTPIN, DHTTYPE); // Cria um objeto DHT
 
 void setup() {
-  pinMode(D4, OUTPUT);
-  dht.begin();
-  delay(2000);
-
-  Serial.begin(115200);
+  Serial.begin(115200);   // Inicializa a comunicação serial com a velocidade de 9600 baud
+  dht.begin();            // Inicializa o sensor DHT11
 }
 
 void loop() {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
+  float h = dht.readHumidity();      // Lê a umidade relativa do ar em porcentagem
+  float t = dht.readTemperature();  // Lê a temperatura em graus Celsius
 
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print("C :");
-  Serial.print(humidity);
-  delay(2000);
+  Serial.print("Umidade: ");
+  Serial.print(h);
+  Serial.print("% - ");
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.println("°C");
+  
+  delay(2000);  // Espera 2 segundos antes de fazer a próxima leitura
 }
