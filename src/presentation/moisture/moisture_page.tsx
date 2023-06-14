d0b79154582f969babf62c51734e99848547e2f4
giogiuseppe <giovannirosa251500@gmail.com>
2023-06-14 17:22:00 -0300

    FEAT: removing carousel and adding list / add hover effect in sidebar

--- src/presentation/moisture/moisture_page.tsx
+++ src/presentation/moisture/moisture_page.tsx
@@ -4,11 +4,6 @@ import GraphicComponent from '../shared/components/home/graphic';
 import CarouselComponent from '../shared/components/home/carousel';
 
 import Logo from '../../assets/images/logo.svg';
-import MoistureIcon from '../../assets/images/icons/moisture.svg';
-import TemperatureIcon from '../../assets/images/icons/temperature.svg';
-import NotificationIcon from '../../assets/images/icons/notification.svg';
-import ProfileIcon from '../../assets/images/icons/profile.svg';
-import LogoutIcon from '../../assets/images/icons/logout.svg';
 
 export const MoistureComponent: React.FC = () => {
 
@@ -17,14 +12,9 @@ export const MoistureComponent: React.FC = () => {
             <NavComponent
                 logo={Logo}
                 navicon1='/umidade'
-                icon1={MoistureIcon}
                 navicon2='/temperatura'
-                icon2={TemperatureIcon}
                 navicon3='/notificacao'
-                icon3={NotificationIcon}
                 navicon4='/perfil'
-                icon4={ProfileIcon}
-                logout={LogoutIcon}
             />
             <GraphicComponent />
             <CarouselComponent
--- src/presentation/notification/notification_page.tsx
+++ src/presentation/notification/notification_page.tsx
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from 'react';
 import NavComponent from '../shared/components/home/nav';
 
 import Logo from '../../assets/images/logo.svg';
-import MoistureIcon from '../../assets/images/icons/moisture.svg';
-import TemperatureIcon from '../../assets/images/icons/temperature.svg';
-import NotificationIcon from '../../assets/images/icons/notification.svg';
-import ProfileIcon from '../../assets/images/icons/profile.svg';
-import LogoutIcon from '../../assets/images/icons/logout.svg';
 
 import TitleComponent from '../shared/components/form/title';
 
@@ -91,14 +86,9 @@ export const NotificationComponent: React.FC = () => {
             <NavComponent
                 logo={Logo}
                 navicon1="/umidade"
-                icon1={MoistureIcon}
                 navicon2="/temperatura"
-                icon2={TemperatureIcon}
                 navicon3="/notificacao"
-                icon3={NotificationIcon}
                 navicon4="/perfil"
-                icon4={ProfileIcon}
-                logout={LogoutIcon}
             />
             <div className="notification-page">
                 <TitleComponent title="Notificações" />
