38f770774d2f022f9cc452c1f38202cef5d8ea79
JeffeVargasP <jeffevargas@proton.me>
2023-06-16 01:16:17 -0300

    procfile

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.SV_PORT || 3002;
+const port = process.env.SV_PORT || 3000;
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log("Server is running!")
