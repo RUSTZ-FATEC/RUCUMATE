309891e745fb7d8038ac4c33b35050d00f83023f
JeffeVargasP <jeffevargas@proton.me>
2023-06-16 01:03:13 -0300

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const port = process.env.SV_PORT || 3000;
+const port = process.env.SV_PORT || 3002;
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log("Server is running!")
