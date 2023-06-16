bbe5570289a426d0004f5ff27f7595a23bac29e8
giogiuseppe <giovannirosa251500@gmail.com>
2023-06-15 23:17:46 -0300

    FIX: alterando funcionalidade de cadastro, (removendo campo de e-mail)

--- src/presentation/signup/signup_page.tsx
+++ src/presentation/signup/signup_page.tsx
@@ -4,15 +4,10 @@ import Logo from "../../assets/images/logo.svg";
 
 export const SignupComponent: React.FC = () => {
 
-    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [showModal, setShowModal] = useState(false);
-
-    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
-    };
 
     const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(event.target.value);
@@ -27,7 +22,7 @@ export const SignupComponent: React.FC = () => {
     };
 
     const handleClick = () => {
-        if (!username || !email || !password) {
+        if (!email || !password) {
             setShowModal(true);
             return;
         }
@@ -40,7 +35,6 @@ export const SignupComponent: React.FC = () => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "username": username,
                 "email": email,
                 "passwd": password
             })
@@ -76,20 +70,6 @@ export const SignupComponent: React.FC = () => {
 
                     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                         <form className="space-y-6" action="#" method="POST">
-                            <div>
-                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
-                                    Usuário
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        value={username}
-                                        onChange={handleUsernameChange}
-                                        required
-                                        className="block outline-none bg-[#202124] text-white w-full rounded-md border-0 py-1.5 px-3 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                                    />
-                                </div>
-                            </div>
-
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
                                     E-mail
